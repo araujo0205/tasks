ffcf6ba1d422f1446db3d86af8b9afb997f2f76e
David Souza <contato@david.tec.br>
2026-05-08 00:23:19 +0000

    Create inbox page and add route

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, FolderGit2, LayoutGrid } from 'lucide-vue-next';
+import { BookOpen, FolderGit2, LayoutGrid, Inbox } from 'lucide-vue-next';
 import AppLogo from '@/components/AppLogo.vue';
 import NavFooter from '@/components/NavFooter.vue';
 import NavMain from '@/components/NavMain.vue';
@@ -14,7 +14,7 @@ import {
     SidebarMenuButton,
     SidebarMenuItem,
 } from '@/components/ui/sidebar';
-import { dashboard } from '@/routes';
+import { dashboard, inbox } from '@/routes';
 import type { NavItem } from '@/types';
 
 const mainNavItems: NavItem[] = [
@@ -23,20 +23,14 @@ const mainNavItems: NavItem[] = [
         href: dashboard(),
         icon: LayoutGrid,
     },
+    {
+        title: 'Adicionar Tarefa',
+        href: inbox(),
+        icon: Inbox,
+    }
 ];
 
-const footerNavItems: NavItem[] = [
-    {
-        title: 'Repository',
-        href: 'https://github.com/laravel/vue-starter-kit',
-        icon: FolderGit2,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#vue',
-        icon: BookOpen,
-    },
-];
+const footerNavItems: NavItem[] = [];
 </script>
 
 <template>
---
+++ resources/js/pages/Inbox.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+import { inbox } from '@/routes';
+import { Textarea } from '@/components/ui/textarea';
+import { Button } from '@/components/ui/button';
+import { ref } from 'vue';
+import { Form } from "@inertiajs/vue3";
+import { store } from "@/actions/App/Http/Controllers/TaskController";
+const task = ref('');
+
+const handleSuccess = () => {
+    task.value = '';
+};
+
+defineOptions({
+    layout: {
+        breadcrumbs: [
+            {
+                title: 'Adicionar Tarefa',
+                href: inbox(),
+            },
+        ],
+    },
+});
+
+</script>
+
+<template>
+    <Head title="Adicionar Tarefa" />
+        <Form method="post" :action="store()" @success="handleSuccess" class="flex items-center justify-center h-screen flex-col gap-4">
+            <Textarea name="title" placeholder="Digite sua tarefa aqui..." v-model="task" class="w-full max-w-md min-h-[200px]" />
+            <Button class="ml-4" type="submit" :disabled="!task">Adicionar</Button>
+        </Form>
+</template>
+
